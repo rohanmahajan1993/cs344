bf54d7429384715a65537b6000df0d9aed311abd
Rohan Mahajan <you@example.com>
2017-08-08 07:14:18 -0700

    checkpoint 1

--- Problem Sets/Problem Set 3/student_func.cu
+++ Problem Sets/Problem Set 3/student_func.cu
@@ -1,6 +1,60 @@
 #include "reference_calc.cpp"
 #include "utils.h"
 
+// Adapted from udacity code snippets
+__global__ void find_optimum(float * d_out, float * d_in, bool isMinimum)
+{
+    int myId = threadIdx.x + blockDim.x * blockIdx.x;
+    int tid  = threadIdx.x;
+
+    // do reduction in global mem
+    for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1)
+    {
+        if (tid < s)
+        {
+	    if (isMinimum) {
+              d_in[myId] = min(d_in[myId + s], d_in[myId]);
+            } else {
+              d_in[myId] = max(d_in[myId + s], d_in[myId]);
+            }
+        }
+        __syncthreads();        // make sure all adds at one stage are done!
+    }
+
+    // only thread 0 writes result for this block back to global mem
+    if (tid == 0)
+    {
+        d_out[blockIdx.x] = d_in[myId];
+    }
+}
+
+float calculateDifference(const float * const h_input, float min_logLum, float max_logLum, int numEntries) {
+    float *h_minValue;
+    float *h_maxValue;
+    float *d_maxValue;
+    float *d_minValue;
+    float *d_input;
+    float *d_out;
+    int threadLimit = 512;
+    int blockWidth = threadLimit;
+    int numBlocks = numEntries / threadLimit + (numBlocks % threadLimit != 0); 
+    checkCudaErrors(cudaMalloc(&d_minValue,   sizeof(float)));
+    checkCudaErrors(cudaMalloc(&d_maxValue,   sizeof(float)));
+    checkCudaErrors(cudaMalloc(&d_input,   sizeof(float) * numEntries));
+    checkCudaErrors(cudaMalloc(&d_out,   sizeof(float) * numBlocks));
+    checkCudaErrors(cudaMemcpy(d_input, h_input, sizeof(float) * numEntries, cudaMemcpyHostToDevice));
+    find_optimum<<<numBlocks, blockWidth>>>(d_input,  d_minValue, true);
+    checkCudaErrors(cudaMemcpy(d_input, h_input, sizeof(float) * numEntries, cudaMemcpyHostToDevice));
+    find_optimum<<<numBlocks, blockWidth>>(d_input,  d_maxValue, false);
+    checkCudaErrors(cudaMemcpy(d_minValue, h_minValue, sizeof(float), cudaMemcpyDeviceToHost));
+    checkCudaErrors(cudaMemcpy(d_maxValue, h_maxValue, sizeof(float), cudaMemcpyDeviceToHost));
+    checkCudaErrors(cudaFree(d_maxValue));
+    checkCudaErrors(cudaFree(d_minValue));
+    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+    float difference = h_maxValue - h_minValue; 
+    return difference;
+}
+
 void your_histogram_and_prefixsum(const float* const d_logLuminance,
                                   unsigned int* const d_cdf,
                                   float &min_logLum,
@@ -26,25 +80,15 @@ void your_histogram_and_prefixsum(const float* const d_logLuminance,
     int numBlockCols = numCols / blockHeight + (numCols % blockHeight != 0);
     const dim3 blockSize(blockWidth, blockHeight, 1);
     const dim3 gridSize(numBlockRows, numBlockCols, 1);
-    float *h_minValue;
-    float *h_maxValue;
-    float *d_maxValue;
-    float *d_minValue;
-    checkCudaErrors(cudaMalloc(&d_minValue,   sizeof(float)));
-    checkCudaErrors(cudaMalloc(&d_maxValue,   sizeof(float)));
-    find_optimum<<<gridSize, blockSize>>>(d_input,  d_minValue, true);
-    find_optimum<<<gridSize, blockSize>>>(d_input,  d_maxValue, false);
-    checkCudaErrors(cudaMemcpy(d_minValue, h_minValue, sizeof(float), cudaMemcpyDeviceToHost));
-    checkCudaErrors(cudaMemcpy(d_maxValue, h_maxValue, sizeof(float), cudaMemcpyDeviceToHost));
-    float difference = h_maxValue - h_minValue;
-    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
-}
+   
+ }
 //This kernel takes in three color channels and recombines them
 //into one image.  The alpha channel is set to 255 to represent
 //that this image has no transparency.
 __global__
 void findOptimum(const float* const d_input, float * optimalValue, boolean isMinimum)
 {
+  int threadLimit = 512
   int rowIndex = threadIdx.x + blockDim.x * blockIdx.x;
   int colIndex = threadIdx.y + blockDim.y * blockIdx.y;
   int index = (rowIndex * numCols) + colIndex;
