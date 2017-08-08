50846572c6b2a3b6ad883fdcdf922bb6f33fb445
Rohan Mahajan <you@example.com>
2017-08-07 17:16:30 -0700

    starting lab 3

--- Problem Sets/Problem Set 3/student_func.cu
+++ Problem Sets/Problem Set 3/student_func.cu
@@ -1,84 +1,4 @@
-/* Udacity Homework 3
-   HDR Tone-mapping
-
-  Background HDR
-  ==============
-
-  A High Dynamic Range (HDR) image contains a wider variation of intensity
-  and color than is allowed by the RGB format with 1 byte per channel that we
-  have used in the previous assignment.  
-
-  To store this extra information we use single precision floating point for
-  each channel.  This allows for an extremely wide range of intensity values.
-
-  In the image for this assignment, the inside of church with light coming in
-  through stained glass windows, the raw input floating point values for the
-  channels range from 0 to 275.  But the mean is .41 and 98% of the values are
-  less than 3!  This means that certain areas (the windows) are extremely bright
-  compared to everywhere else.  If we linearly map this [0-275] range into the
-  [0-255] range that we have been using then most values will be mapped to zero!
-  The only thing we will be able to see are the very brightest areas - the
-  windows - everything else will appear pitch black.
-
-  The problem is that although we have cameras capable of recording the wide
-  range of intensity that exists in the real world our monitors are not capable
-  of displaying them.  Our eyes are also quite capable of observing a much wider
-  range of intensities than our image formats / monitors are capable of
-  displaying.
-
-  Tone-mapping is a process that transforms the intensities in the image so that
-  the brightest values aren't nearly so far away from the mean.  That way when
-  we transform the values into [0-255] we can actually see the entire image.
-  There are many ways to perform this process and it is as much an art as a
-  science - there is no single "right" answer.  In this homework we will
-  implement one possible technique.
-
-  Background Chrominance-Luminance
-  ================================
-
-  The RGB space that we have been using to represent images can be thought of as
-  one possible set of axes spanning a three dimensional space of color.  We
-  sometimes choose other axes to represent this space because they make certain
-  operations more convenient.
-
-  Another possible way of representing a color image is to separate the color
-  information (chromaticity) from the brightness information.  There are
-  multiple different methods for doing this - a common one during the analog
-  television days was known as Chrominance-Luminance or YUV.
-
-  We choose to represent the image in this way so that we can remap only the
-  intensity channel and then recombine the new intensity values with the color
-  information to form the final image.
-
-  Old TV signals used to be transmitted in this way so that black & white
-  televisions could display the luminance channel while color televisions would
-  display all three of the channels.
-  
-
-  Tone-mapping
-  ============
-
-  In this assignment we are going to transform the luminance channel (actually
-  the log of the luminance, but this is unimportant for the parts of the
-  algorithm that you will be implementing) by compressing its range to [0, 1].
-  To do this we need the cumulative distribution of the luminance values.
-
-  Example
-  -------
-
-  input : [2 4 3 3 1 7 4 5 7 0 9 4 3 2]
-  min / max / range: 0 / 9 / 9
-
-  histo with 3 bins: [4 7 3]
-
-  cdf : [4 11 14]
-
-
-  Your task is to calculate this cumulative distribution by following these
-  steps.
-
-*/
-
+#include "reference_calc.cpp"
 #include "utils.h"
 
 void your_histogram_and_prefixsum(const float* const d_logLuminance,
@@ -99,6 +19,48 @@ void your_histogram_and_prefixsum(const float* const d_logLuminance,
     4) Perform an exclusive scan (prefix sum) on the histogram to get
        the cumulative distribution of luminance values (this should go in the
        incoming d_cdf pointer which already has been allocated for you)       */
+    int threadLimit = 512;
+    int blockWidth = trunc(sqrt(threadLimit));
+    int blockHeight = trunc(sqrt(threadLimit));
+    int numBlockRows = numRows / blockWidth + (numRows % blockWidth != 0);
+    int numBlockCols = numCols / blockHeight + (numCols % blockHeight != 0);
+    const dim3 blockSize(blockWidth, blockHeight, 1);
+    const dim3 gridSize(numBlockRows, numBlockCols, 1);
+    float *h_minValue;
+    float *h_maxValue;
+    float *d_maxValue;
+    float *d_minValue;
+    checkCudaErrors(cudaMalloc(&d_minValue,   sizeof(float)));
+    checkCudaErrors(cudaMalloc(&d_maxValue,   sizeof(float)));
+    find_optimum<<<gridSize, blockSize>>>(d_input,  d_minValue, true);
+    find_optimum<<<gridSize, blockSize>>>(d_input,  d_maxValue, false);
+    checkCudaErrors(cudaMemcpy(d_minValue, h_minValue, sizeof(float), cudaMemcpyDeviceToHost));
+    checkCudaErrors(cudaMemcpy(d_maxValue, h_maxValue, sizeof(float), cudaMemcpyDeviceToHost));
+    float difference = h_maxValue - h_minValue;
+    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+}
+//This kernel takes in three color channels and recombines them
+//into one image.  The alpha channel is set to 255 to represent
+//that this image has no transparency.
+__global__
+void findOptimum(const float* const d_input, float * optimalValue, boolean isMinimum)
+{
+  int rowIndex = threadIdx.x + blockDim.x * blockIdx.x;
+  int colIndex = threadIdx.y + blockDim.y * blockIdx.y;
+  int index = (rowIndex * numCols) + colIndex;
+  if (rowIndex < numRows && colIndex < numCols) {
+    unsigned char red   = redChannel[index];
+    unsigned char green = greenChannel[index];
+    unsigned char blue  = blueChannel[index];
+    //Alpha should be 255 for no transparency
+    uchar4 outputPixel = make_uchar4(red, green, blue, 255);
+    outputImageRGBA[index] = outputPixel;
+  }
+}
 
-
+void cleanup() {
+  checkCudaErrors(cudaFree(d_red));
+  checkCudaErrors(cudaFree(d_green));
+  checkCudaErrors(cudaFree(d_blue));
+  checkCudaErrors(cudaFree(d_filter));
 }
